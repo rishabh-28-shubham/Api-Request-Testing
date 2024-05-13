b2103d3650529a162c943c3e71e8f5a5dda587fe
Rishabh Vishwakarma <ectopdash@gmail.com>
2024-05-13 11:18:33 +0530

    constrainst++ | api Request ✔

---
+++ constrains.js
@@ -0,0 +1,5 @@
+const name = document.querySelector('#name') //name
+const bio = document.querySelector('#bio') //bio
+const profile = document.querySelector('#avatar') //profile as avatar
+const userId = document.querySelector('#login') //user id
+const location = document.querySelector('#')
--- get.js
+++ get.js
@@ -1,12 +1,13 @@
-const requestUrl = 'https://api.github.com/users/mrunmay-24-ukey';
+const requestUrl = 'https://api.github.com/users/rishabh-28-shubham';
 const xhr = new XMLHttpRequest();
 xhr.open('GET', requestUrl)
 xhr.onreadystatechange = function () {
-    console.log(xhr.readyState);
+    // console.log(xhr.readyState);
         if(xhr.readyState === 4){
         const data = JSON.parse(this.responseText)
-        console.log(typeof data)
-        console.log(data.followers)
+        // console.log(typeof data)
+            // console.log(data.name)
+        
     }
 }
 xhr.send()
